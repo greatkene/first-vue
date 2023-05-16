8eedb04953a0a59b494c867a3cc8f545fd6b759d
greatkene <ihevuemeg@gmail.com>
2023-05-16 13:26:43 +0100

    first step to greatness 🥳

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue"
+import "materialize-css/dist/css/materialize.min.css"
+import "materialize-css/dist/js/materialize.min.js"
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+</style>
